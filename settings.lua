5a53252dc2b8e713c3cbfa382b82d7e17cbe1b30
Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-03-23 12:20:29 -0500

    Removed default depenency.
    Removed duplicate settings from ambient_sounds namespace.
    Removed "debug" from core.log internal.lua due to "debug" not being a reconized log level.
    Changed default to an optional_depends in mod.conf.
    Added if checks for if default is loaded.
    Added settingtypes.txt for a settings menu in luanti UI.
    Added calls to core.settings for settings defined in settingtypes.txt.
    
    Signed-off-by: Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>

--- settings.lua
+++ settings.lua
@@ -1,20 +1,17 @@
--- Enable debug logging
-ambient_sounds.debug_mode = false
-
--- Interval for checking the environment (in seconds)
-ambient_sounds.check_interval = 2.0
-
--- Nodes detection radius 
--- Tune for performace, may cause performance issues if 
--- too high and many players are connected
--- 8-15 is a good range
-ambient_sounds.detection_radius = 15
-
--- Time for fading in/out when switching sounds (in seconds)
-ambient_sounds.fade_time = 4.0
-
--- Default gain
-gain = ambient_sounds.gain or 0.5
-
--- Ocean water count threshold (tune with detection radius!!)
-ambient_sounds.ocean_water_count = 200
+ambient_sounds.config = {
+        -- Enable debug logging
+    debug_mode = core.settings:get_bool("ambient_sounds_debug") or false,
+        -- Interval for checking the environment (in seconds)
+    check_interval = tonumber(core.settings:get("ambient_sounds_interval")) or 2.0,
+        -- Nodes detection radius 
+        -- Tune for performace, may cause performance issues if 
+        -- too high and many players are connected
+        -- 8-15 is a good range
+    detection_radius = tonumber(core.settings:get("ambient_sounds_detection")) or 15,
+        -- Time for fading in/out when switching sounds (in seconds)
+    fade_time = tonumber(core.settings:get("ambient_sounds_fade")) or 4.0,
+        -- Default gain
+    gain = tonumber(core.settings:get("ambient_sounds_gain")) or 0.5,
+        -- Ocean water count threshold (tune with detection radius!!)
+    ocean_water_count = tonumber(core.settings:get("ambient_sounds_water")) or 200
+}
